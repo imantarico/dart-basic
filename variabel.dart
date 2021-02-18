33f6578da61bbc25fae273d5b9823a1274580c80
imantarico <icoversi90@gmail.com>
2021-02-18 08:05:08 +0700

    Menambahkan variabel.dart

---
+++ variabel.dart
@@ -0,0 +1,12 @@
+void main() {
+  String nama = "Dian";
+  int umur = 23;
+  double tinggi = 180.43;
+  bool isMenikah = false;
+
+  var alamat = "Lombok, Indonesia";
+
+  print("Nama saya $nama. Umur $umur tahun. Tinggi sekitar $tinggi cm.");
+  print("Menikah: $isMenikah");
+  print("Alamat : $alamat");
+}
